3c35103138a25e9f76068458cfce0241104ca913
VALERA771 <val.germanov@gmail.com>
2024-11-27 23:23:31 +0300

    settings

---
+++ EXILED/Exiled.API/Features/Core/UserSettings/SettingBase.cs
@@ -0,0 +1,52 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="SettingBase.cs" company="ExMod Team">
+// Copyright (c) ExMod Team. All rights reserved.
+// Licensed under the CC BY-SA 3.0 license.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System.Collections.Generic;
+
+namespace Exiled.API.Features.Core.UserSettings
+{
+    using global::UserSettings.ServerSpecific;
+
+    public class SettingBase
+    {
+        internal static Dictionary<ServerSpecificSettingBase, SettingBase> Settings = new();
+
+        public SettingBase(ServerSpecificSettingBase settingBase)
+        {
+            Base = settingBase;
+
+            Settings.Add(settingBase, this);
+        }
+
+        public ServerSpecificSettingBase Base { get; }
+
+        public int Id
+        {
+            get => Base.SettingId;
+            set => Base.SetId(value, string.Empty);
+        }
+
+        public string Label
+        {
+            get => Base.Label;
+            set => Base.Label = value;
+        }
+
+        public string HintDescription
+        {
+            get => Base.HintDescription;
+            set => Base.HintDescription = value;
+        }
+
+        public ServerSpecificSettingBase.UserResponseMode ResponseMode => Base.ResponseMode;
+
+        public static bool TryGetSetting<T>(Player player, int id, out T setting)
+            where T : SettingBase
+                => (setting = ServerSpecificSettingsSync.TryGetSettingOfUser(player.ReferenceHub, id, out ServerSpecificSettingBase result) && Settings.TryGetValue(result, out SettingBase settingBase)
+                    ? (T)settingBase : null) != null;
+    }
+}
---
+++ EXILED/Exiled.API/Features/Core/UserSettings/TwoButtonsSetting.cs
@@ -0,0 +1,50 @@
+﻿using UserSettings.ServerSpecific;
+
+namespace Exiled.API.Features.Core.UserSettings
+{
+    public class TwoButtonsSetting : SettingBase
+    {
+        public TwoButtonsSetting(SSTwoButtonsSetting settingBase)
+            : base(settingBase)
+        {
+            Base = settingBase;
+        }
+
+        public TwoButtonsSetting(int id, string label, string firstOption, string secondOption, bool defaultIsSecond = false, string hintDescription = "")
+            : this(new SSTwoButtonsSetting(id, label, firstOption, secondOption, defaultIsSecond, hintDescription))
+        {
+        }
+
+        public new SSTwoButtonsSetting Base { get; }
+
+        public bool IsSecond
+        {
+            get => Base.SyncIsB;
+            set => Base.SyncIsB = value;
+        }
+
+        public bool IsFirst
+        {
+            get => Base.SyncIsA;
+            set => Base.SyncIsB = !value;
+        }
+
+        public bool IsSecondDefalut
+        {
+            get => Base.DefaultIsB;
+            set => Base.DefaultIsB = value;
+        }
+
+        public string FirstOption
+        {
+            get => Base.OptionA;
+            set => Base.OptionA = value;
+        }
+
+        public string SecondOption
+        {
+            get => Base.OptionB;
+            set => Base.OptionB = value;
+        }
+    }
+}
