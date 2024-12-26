b32734e1ce5afe2f856d254e0b68d0ebb77521b5
Yamato <louismonneyron5@yahoo.com>
2024-12-26 19:43:12 +0100

    ExMod Team copyright

--- EXILED/Exiled.CustomItems/API/Features/CustomKeycard.cs
+++ EXILED/Exiled.CustomItems/API/Features/CustomKeycard.cs
@@ -1,6 +1,6 @@
-﻿// -----------------------------------------------------------------------
-// <copyright file="CustomKeycard.cs" company="Exiled Team">
-// Copyright (c) Exiled Team. All rights reserved.
+// -----------------------------------------------------------------------
+// <copyright file="CustomKeycard.cs" company="ExMod Team">
+// Copyright (c) ExMod Team. All rights reserved.
 // Licensed under the CC BY-SA 3.0 license.
 // </copyright>
 // -----------------------------------------------------------------------
